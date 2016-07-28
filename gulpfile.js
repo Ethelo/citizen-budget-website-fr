e75870b65fdb552c2f1ec8294e450d68a52a27f2
Guilherme Mattar Bastos <guilhermembastos@gmail.com>
2016-07-28 17:28:07 -0300

    Compile templates and improve gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,30 @@
 var nunjucksRender = require('gulp-nunjucks-render'),
     browserSync    = require('browser-sync'),
-    imagemin       = require('gulp-imagemin'),
-    cache          = require('gulp-cache'),
+    runSequence    = require('run-sequence'),
+    ghPages        = require('gulp-gh-pages'),
+    gutil          = require('gulp-util'),
     shell          = require('gulp-shell'),
-    data           = require('gulp-data'),
+    file           = require('gulp-file'),
     gulp           = require('gulp'),
     sass           = require('gulp-sass'),
     del            = require('del');
 
 // Clean Dist
 gulp.task('clean', function() {
-  return del.sync('public');
+  return del('public');
 });
 
 // Browser Sync
 gulp.task('browserSync', function() {
   browserSync({
-    server: {
-      baseDir: 'public'
-    }
+    server: { baseDir: 'public' },
+    startPath: '/en'
   });
 });
 
 // Sass
 gulp.task('sass', function() {
-  return gulp.src('source/sass/**/*.scss') // Gets all files ending with .scss in source/sass
+  return gulp.src('source/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('public/en'))
     .pipe(gulp.dest('public/fr'))
@@ -49,12 +49,22 @@ gulp.task('js-dev', function() {
     .pipe(gulp.dest('public/fr'));
 });
 
+gulp.task('cname-en', function() {
+  return file('CNAME', 'www.citizenbudget.com\n', { src: true })
+    .pipe(gulp.dest('public/en'));
+});
+
+gulp.task('cname-fr', function() {
+  return file('CNAME', 'www.budgetcitoyen.com\n', { src: true })
+    .pipe(gulp.dest('public/fr'));
+});
+
 // Nunjucks
 gulp.task('nunjucks-en', function() {
   // Gets .html and .nunjucks files in pages
   return gulp.src('source/templates/en/**/[^_]*.html')
     // Renders template with nunjucks
-    .pipe(nunjucksRender({path: 'source/templates/en'}))
+    .pipe(nunjucksRender({ path: 'source/templates/en' }))
     // output files in app folder
     .pipe(gulp.dest('public/en'))
     .pipe(browserSync.reload({ stream: true }));
@@ -64,22 +74,54 @@ gulp.task('nunjucks-fr', function() {
   // Gets .html and .nunjucks files in pages
   return gulp.src('source/templates/fr/**/[^_]*.html')
     // Renders template with nunjucks
-    .pipe(nunjucksRender({path: ['source/templates/fr']}))
+    .pipe(nunjucksRender({ path: 'source/templates/fr' }))
     // output files in app folder
     .pipe(gulp.dest('public/fr'))
     .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('deploy', shell.task([
-  'git push cb-en master',
-  'git push cb-fr master',
-  'git push cb-en --delete gh-pages',
-  'git push cb-fr --delete gh-pages',
-  'git subtree push --prefix public/en cb-en gh-pages',
-  'git subtree push --prefix public/fr cb-fr gh-pages'
+gulp.task('push-gh-master-en', shell.task([
+  '=== pushing to cb-en master ===',
+  'git push cb-en master'
 ]));
 
-gulp.task('watch', ['browserSync', 'sass', 'nunjucks-en', 'nunjucks-fr', 'image-dev', 'vendor', 'js-dev'], function() {
+gulp.task('push-gh-master-fr', shell.task([
+  '=== pushing to cb-fr master ===',
+  'git push cb-fr master'
+]));
+
+gulp.task('push-gh-pages-en', function() {
+  var remote = 'git@github.com:opennorth/citizen-budget-website.git';
+
+  gutil.log('=== pushing to cb-en gh-pages ===');
+
+  return gulp.src('public/en/**/*')
+    .pipe(ghPages({ remoteUrl: remote, force: true }));
+});
+
+gulp.task('push-gh-pages-fr', function() {
+  var remote = 'git@github.com:opennorth/citizen-budget-website-fr.git';
+
+  gutil.log('=== pushing to cb-fr gh-pages ===');
+
+  return gulp.src('public/fr/**/*')
+    .pipe(ghPages({ remoteUrl: remote, force: true }));
+});
+
+gulp.task('deploy', function(callback) {
+  runSequence(
+    'clean',
+    ['sass', 'nunjucks-en', 'nunjucks-fr', 'image-dev', 'vendor', 'js-dev'],
+    ['cname-en', 'cname-fr'],
+    'push-gh-master-en',
+    'push-gh-pages-en',
+    'push-gh-master-fr',
+    'push-gh-pages-fr',
+    callback
+  );
+});
+
+gulp.task('watch-dev', function() {
   gulp.watch('source/sass/**/*.scss', ['sass']);
   gulp.watch('source/static/scripts.js', ['js-dev']);
   gulp.watch('source/templates/en/**/*.html', ['nunjucks-en']);
@@ -87,4 +129,13 @@ gulp.task('watch', ['browserSync', 'sass', 'nunjucks-en', 'nunjucks-fr', 'image-
   // Other watchers
 });
 
-gulp.task('default', ['watch']);
+gulp.task('run', function(callback) {
+  runSequence(
+    'clean',
+    ['sass', 'nunjucks-en', 'nunjucks-fr', 'image-dev', 'vendor', 'js-dev'],
+    ['cname-en', 'cname-fr', 'browserSync', 'watch-dev'],
+    callback
+  );
+});
+
+gulp.task('default', ['run']);
